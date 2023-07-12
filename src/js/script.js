c7f540d6c8f38068f0e219d1c2d8b5aaac8adaac
Efflerek <piotr.effler@gmail.com>
2023-07-12 17:19:16 +0100

    Changes in style.css , add model-viewer

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+// Poczekaj, aż dokument HTML zostanie w pełni załadowany
+document.addEventListener('DOMContentLoaded', function() {
+    // Znajdź wszystkie wystąpienia komponentu Model Viewer na stronie
+    const modelViewers = document.querySelectorAll('model-viewer');
+  
+    // Iteruj przez każdy komponent Model Viewer
+    modelViewers.forEach(function(modelViewer) {
+      // Sprawdź, czy komponent jest załadowany
+      modelViewer.addEventListener('load', function() {
+        // Wykonaj dowolne czynności po załadowaniu modelu
+        console.log('Model został załadowany!');
+      });
+  
+      // Sprawdź, czy wystąpił błąd podczas ładowania modelu
+      modelViewer.addEventListener('error', function(event) {
+        // Wyświetl komunikat o błędzie
+        console.error('Wystąpił błąd podczas ładowania modelu:', event);
+      });
+    });
+  });
